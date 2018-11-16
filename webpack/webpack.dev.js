2fa59697b03552cbb2462b03d2e1afdda99f5321
dudarmykola <dudar.mykola.stepan@gmail.com>
2018-11-17 00:59:25 +0200

    fix: manifest, favicons

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, {mode}) => {
   const devMode = mode !== 'production';
@@ -60,6 +61,12 @@ module.exports = (env, {mode}) => {
       new HtmlWebpackPlugin({
         template: path.resolve('./index.html')
       }),
+      new CopyWebpackPlugin([
+        {
+          from: './src/assets/favicon/favicon.ico',
+          to: './assets/favicon'
+        }
+      ]),
       new MiniCssExtractPlugin({
         filename: devMode ? '[name].css' : '[name].[hash].css',
         chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
@@ -71,13 +78,65 @@ module.exports = (env, {mode}) => {
         description: 'You can create your own collections',
         background_color: '#4caf50',
         theme_color: '#4caf50',
+        includeDirectory: true,
+        inject: true,
+        ios: {
+          'apple-mobile-web-app-title': 'CollectionCollector',
+          'apple-mobile-web-app-status-bar-style': 'black-translucent'
+        },
         icons: [
           {
-            src: path.resolve('src/assets/img/coll-coll.svg'),
-            sizes: [96, 128, 192, 256, 384, 512]
+            src: path.resolve('src/assets/favicon/favicon-16x16.png'),
+            sizes: '16x16'
+          },
+          {
+            src: path.resolve('src/assets/favicon/favicon-32x32.png'),
+            sizes: '32x32'
+          },
+          {
+            src: path.resolve('src/assets/favicon/favicon-32x32.png'),
+            sizes: '70x70'
+          },
+          {
+            src: path.resolve('src/assets/favicon/mstile-144x144.png'),
+            sizes: '144x144'
+          },
+          {
+            src: path.resolve('src/assets/favicon/mstile-150x150.png'),
+            sizes: '150x150'
+          },
+          {
+            src: path.resolve('src/assets/favicon/mstile-310x150.png'),
+            sizes: '310x150'
+          },
+          {
+            src: path.resolve('src/assets/favicon/mstile-310x310.png'),
+            sizes: '310x310'
+          },
+          {
+            src: path.resolve('src/assets/favicon/android-chrome-512x512.png'),
+            sizes: '512x512',
+            destination: path.join('icons', 'android')
+          },
+          {
+            src: path.resolve('src/assets/favicon/android-chrome-192x192.png'),
+            sizes: '192x192',
+            destination: path.join('icons', 'android')
+          },
+          {
+            src: path.resolve('src/assets/favicon/apple-touch-icon.png'),
+            sizes: '180x180',
+            destination: path.join('icons', 'ios'),
+            ios: true
+          },
+          {
+            src: path.resolve('src/assets/favicon/safari-pinned-tab.svg'),
+            size: 1024,
+            destination: path.join('icons', 'ios'),
+            ios: 'startup'
           }
-        ],
-        includeDirectory: true
+
+        ]
       })
     ],
     devServer: {
